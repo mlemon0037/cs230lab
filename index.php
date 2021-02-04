f62152f4ee0bfec1c447b918e7709c0a9d0c28aa
mlemon0037 <mal0037@mix.wvu.edu>
2021-02-03 23:22:13 -0500

    Lab 2 about page

---
+++ index.php
@@ -0,0 +1,46 @@
+<main>
+    <link rel="stylesheet" href="css/about.css"/>
+
+    <div class="about">
+        <h1>About Us</h1>
+        <p>We sell really real car parts that are all totally real.</p>
+    </div>
+    <div class="row">
+        <div class="column">
+            <div class="card">
+                <img src="images/george_washington.jpg"/>
+                <div class="container">
+                    <h2>George Washington</h2>
+                    <p>Full-time car part salesman with 200+ years of experience. Won wars on the side.</p>
+                </div>
+            </div>
+        </div>
+        <div class="column">
+            <div class="card">
+                <img src="images/millard_fillmore.jpg"/>
+                <div class="container">
+                    <h2>Millard Fillmore</h2>
+                    <p>Former president who now sells our really real car parts.</p>
+                </div>
+            </div>
+        </div>
+        <div class="column">
+            <div class="card">
+                <img src="images/thomas_jefferson.jpg"/>
+                <div class="container">
+                    <h2>Thomas Jefferson</h2>
+                    <p>Primary writer for the Declaration of Independence and writes our legal paperwork.</p>
+                </div>
+            </div>
+        </div>
+        <div class="column">
+            <div class="card">
+                <img src="images/abraham_lincoln.jpg"/>
+                <div class="container">
+                    <h2>Abraham Lincoln</h2>
+                    <p>He's really just here because we like his big hat.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
